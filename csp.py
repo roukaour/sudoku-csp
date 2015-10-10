c1b8e06ca725d358557412bfc99c4bf0c301d1b0
Remy Oukaour <remy.oukaour@gmail.com>
2015-10-10 19:32:22 -0400

    Code cleanup (use terser functions, e.g. all and rjust)

--- csp.py
+++ csp.py
@@ -1,3 +1,4 @@
+from itertools import product
 
 import gameNode
 
@@ -6,98 +7,83 @@ import gameNode
 ###########################################
 
 def backtrackingHelper(node):
-    if node.solved():
-        # node.p()
-        return True
-
-    for i in range(node.size):
-        for j in range(node.size):
-            if node.gameStatus[i][j] == 0:
-                valid_values = node.get_valid_moves(i, j)
-                for k in valid_values:
-                    node.set_value(i, j, k)
-                    if backtrackingHelper(node):
-                        return True
-                    node.set_empty(i, j)
-
-                return False
-    return False
-
+	if node.solved():
+		# print node
+		return True
+	for i, j in product(xrange(node.size), xrange(node.size)):
+		if node.gameStatus[i][j] != 0:
+			continue
+		for k in node.get_valid_moves(i, j):
+			node.set_value(i, j, k)
+			if backtrackingHelper(node):
+				return True
+			node.set_empty(i, j)
+		return False
+	return False
 
 def backtracking(filename):
-    ###
-    # use backtracking to solve sudoku puzzle here,
-    # return the solution in the form of list of 
-    # list as describe in the PDF with # of consistency
-    # checks done
-    ###
-
-    node = gameNode.gameNode()
-    if not node.load_game(filename):
-        return ("Error: Fail Load", 0)
-
-    if backtrackingHelper(node):
-        return node.solution()
-
-    return ("Error: No Solution", 0)
+	###
+	# use backtracking to solve sudoku puzzle here,
+	# return the solution in the form of list of
+	# list as describe in the PDF with # of consistency
+	# checks done
+	###
+	node = gameNode.gameNode()
+	if not node.load_game(filename):
+		return ("Error: Fail Load", 0)
+	if backtrackingHelper(node):
+		return node.solution()
+	return ("Error: No Solution", 0)
 
 def backtrackingMRVHelper(node):
-    return False
+	return False
 
 def backtrackingMRV(filename):
-    ###
-    # use backtracking + MRV to solve sudoku puzzle here,
-    # return the solution in the form of list of 
-    # list as describe in the PDF with # of consistency
-    # checks done
-    ###
-
-    node = gameNode.gameNode()
-    if not node.load_game(filename):
-        return ("Error: Fail Load", 0)
-
-    if backtrackingMRV(node):
-        return node.solution()
-
-    return ("Error: No Solution", 0)
+	###
+	# use backtracking + MRV to solve sudoku puzzle here,
+	# return the solution in the form of list of
+	# list as describe in the PDF with # of consistency
+	# checks done
+	###
+	node = gameNode.gameNode()
+	if not node.load_game(filename):
+		return ("Error: Fail Load", 0)
+	if backtrackingMRV(node):
+		return node.solution()
+	return ("Error: No Solution", 0)
 
 def backtrackingMRVfwdHelper(node):
-    return False
+	return False
 
 def backtrackingMRVfwd(filename):
-    ###
-    # use backtracking +MRV + forward propogation
-    # to solve sudoku puzzle here,
-    # return the solution in the form of list of 
-    # list as describe in the PDF with # of consistency
-    # checks done
-    ###
-
-    node = gameNode.gameNode()
-    if not node.load_game(filename):
-        return ("Error: Fail Load", 0)
-
-    if backtrackingMRVfwdHelper(node):
-        return node.solution()
-    
-    return ([[],[]], 0)
+	###
+	# use backtracking +MRV + forward propogation
+	# to solve sudoku puzzle here,
+	# return the solution in the form of list of
+	# list as describe in the PDF with # of consistency
+	# checks done
+	###
+	node = gameNode.gameNode()
+	if not node.load_game(filename):
+		return ("Error: Fail Load", 0)
+	if backtrackingMRVfwdHelper(node):
+		return node.solution()
+	return ([[],[]], 0)
 
 def backtrackingMRVcp(filename):
-    ###
-    # use backtracking + MRV + cp to solve sudoku puzzle here,
-    # return the solution in the form of list of 
-    # list as describe in the PDF with # of consistency
-    # checks done
-    ###
-    
-    return ([[],[]], 0)
+	###
+	# use backtracking + MRV + cp to solve sudoku puzzle here,
+	# return the solution in the form of list of
+	# list as describe in the PDF with # of consistency
+	# checks done
+	###
+	return ([[],[]], 0)
 
 def minConflict(filename):
-    ###
-    # use minConflict to solve sudoku puzzle here,
-    # return the solution in the form of list of 
-    # list as describe in the PDF with # of consistency
-    # checks done
-    ###
-    
-    return ([[]], 0)
+	###
+	# use minConflict to solve sudoku puzzle here,
+	# return the solution in the form of list of
+	# list as describe in the PDF with # of consistency
+	# checks done
+	###
+	return ([[]], 0)
--- gameNode.py
+++ gameNode.py
@@ -1,75 +1,64 @@
-
 class gameNode():
-    def __init__(self):
-        self.gameStatus = None
-        self.num_check = 0
 
-        self.size = 0
-        self.height = 0
-        self.width = 0
+	def __init__(self):
+		self.gameStatus = None
+		self.num_check = 0
+		self.size = 0
+		self.height = 0
+		self.width = 0
 
-    def load_game(self, filename):
-        assert(self.gameStatus is None)
-        self.gameStatus = []
-        try:
-            with open(filename, 'r') as f:
-                firstline = f.readline()
-                self.size, self.height, self.width = [int(x) for x in firstline.strip()[:-1].split(',')]
-                assert(self.size == self.height * self.width)
-                for i in range(self.size):
-                    line = f.readline()
-                    elements = line.strip()[:-1].split(',')
-                    elements = [0 if x == '-' else int(x) for x in elements]
-                    self.gameStatus.append(elements)
+	def __str__(self):
+		return '\n'.join(' '.join(str(x).rjust(2) for x in self.gameStatus[i])
+			for i in xrange(self.size))
 
-        except IOError as e:
-            print("Error:", e)
-            print("Error: fail to load the testcase from file %s" % (filename))
-            self.gameStatus = None
-            return False
+	def load_game(self, filename):
+		assert self.gameStatus is None
+		self.gameStatus = []
+		try:
+			f = open(filename, 'r')
+			metadata = f.readline().strip().rstrip(';').split(',')
+			self.size, self.height, self.width = (int(t) for t in metadata)
+			assert self.size == self.height * self.width
+			for i in xrange(self.size):
+				line = f.readline()
+				elements = line.strip().rstrip(';').split(',')
+				elements = [0 if x == '-' else int(x) for x in elements]
+				self.gameStatus.append(elements)
+			f.close()
+			return True
+		except IOError as e:
+			print("Error:", e)
+			print("Error: fail to load the testcase from file %s" % filename)
+			self.gameStatus = None
+			return False
 
-        return True
+	def solved(self):
+		return all(self.gameStatus[i][j] != 0
+			for i in xrange(self.size) for j in xrange(self.size))
 
-    def solved(self):
-        for i in range(self.size):
-            for j in range(self.size):
-                if self.gameStatus[i][j] == 0:
-                    return False
-        return True
+	def has_load_game(self):
+		return self.gameStatus is not None
 
-    def has_load_game(self):
-        return self.gameStatus is not None
+	def get_valid_moves(self, i, j):
+		assert self.has_load_game()
+		if self.gameStatus[i][j] != 0:
+			return []
+		self.num_check += 1
+		invalid_moves = set()
+		# block
+		bi = i // self.height * self.height
+		bj = j // self.width * self.width
+		for k in xrange(self.size):
+			invalid_moves.add(self.gameStatus[i][k])
+			invalid_moves.add(self.gameStatus[k][j])
+			invalid_moves.add(self.gameStatus[bi+k//self.width][bj+k%self.width])
+		return (m for m in xrange(1, self.size+1) if m not in invalid_moves)
 
-    def get_valid_moves(self, i, j):
-        assert(self.has_load_game())
-        if self.gameStatus[i][j] != 0:
-            return []
+	def set_value(self, i, j, v):
+		self.gameStatus[i][j] = v
 
-        self.num_check += 1
+	def set_empty(self, i, j):
+		self.gameStatus[i][j] = 0
 
-        unvalid_moves = set()
-
-        # block
-        block_i = i // self.height * self.height
-        block_j = j // self.width * self.width
-
-        for x in range(self.size):
-            unvalid_moves.add(self.gameStatus[i][x])
-            unvalid_moves.add(self.gameStatus[x][j])
-            unvalid_moves.add(self.gameStatus[block_i+x//self.width][block_j+x%self.width])
-
-        valid_moves = [x for x in range(self.size+1) if x not in unvalid_moves]
-        return valid_moves
-
-    def set_value(self, i, j, v):
-        self.gameStatus[i][j] = v
-
-    def set_empty(self, i, j):
-        self.gameStatus[i][j] = 0
-
-    def p(self):
-        for i in range(self.size):
-            print ' '.join([(' ' + str(x)) if x < 10 else str(x) for x in self.gameStatus[i]])
-
-    def solution(self):
-        return (self.gameStatus, self.num_check)
+	def solution(self):
+		return (self.gameStatus, self.num_check)
