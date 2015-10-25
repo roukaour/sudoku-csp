2d8b214f3cfb6e7f9b264753b677f9ecb08f2b50
Remy Oukaour <remy.oukaour@gmail.com>
2015-10-25 13:47:14 -0400

    Update test case data

--- sudoku.py
+++ sudoku.py
@@ -6,75 +6,75 @@ import time
 # NOTE: Don't edit this file
 ###############################################################
 def main(args):
-    ###
-    # args.input: this will give you the path to game.txt file
-    ###
-    
-    ##########################################################
-    # backtracking
-    ##########################################################
+	###
+	# args.input: this will give you the path to game.txt file
+	###
+
+	##########################################################
+	# backtracking
+	##########################################################
 	tic = time.clock()
 	solution, consistencyChecks = csp.backtracking(args.input)
 	toc = time.clock()
 	timeItr = toc - tic
-	
+
 	print "Backtracking:"
 	print "Execution Time: " + str(timeItr)
 	print "Consistency Checks: " + str(consistencyChecks)
 	print "Solution: " + str(solution) + '\n'
-    
-    ##########################################################
-    # backtracking + MRV
-    ##########################################################
+
+	##########################################################
+	# backtracking + MRV
+	##########################################################
 	tic = time.clock()
 	solution, consistencyChecks = csp.backtrackingMRV(args.input)
 	toc = time.clock()
 	timeItr = toc - tic
-	
+
 	print "backtrackingMRV:"
 	print "Execution Time: " + str(timeItr)
 	print "Consistency Checks: " + str(consistencyChecks)
 	print "Solution: " + str(solution) + '\n'
-    
-    ##########################################################
-    # backtracking + MRV + fwd
-    ##########################################################
+
+	##########################################################
+	# backtracking + MRV + fwd
+	##########################################################
 	tic = time.clock()
 	solution, consistencyChecks = csp.backtrackingMRVfwd(args.input)
 	toc = time.clock()
 	timeItr = toc - tic
-	
+
 	print "backtrackingMRVfwd:"
 	print "Execution Time: " + str(timeItr)
 	print "Consistency Checks: " + str(consistencyChecks)
 	print "Solution: " + str(solution) + '\n'
-    
-    ##########################################################
-    # backtracking + MRV + CP
-    ##########################################################
+
+	##########################################################
+	# backtracking + MRV + CP
+	##########################################################
 	tic = time.clock()
 	solution, consistencyChecks = csp.backtrackingMRVcp(args.input)
 	toc = time.clock()
 	timeItr = toc - tic
-	
+
 	print "backtrackingMRVcp:"
 	print "Execution Time: " + str(timeItr)
 	print "Consistency Checks: " + str(consistencyChecks)
 	print "Solution: " + str(solution) + '\n'
-    
-    ##########################################################
-    # minConflict
-    ##########################################################
+
+	##########################################################
+	# minConflict
+	##########################################################
 	tic = time.clock()
 	solution, consistencyChecks = csp.minConflict(args.input)
 	toc = time.clock()
 	timeItr = toc - tic
-	
+
 	print "minConflict:"
 	print "Execution Time: " + str(timeItr)
 	print "Consistency Checks: " + str(consistencyChecks)
 	print "Solution: " + str(solution) + '\n'
-    
+
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="HomeWork Three")
